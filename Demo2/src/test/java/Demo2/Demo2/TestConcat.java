d99f99094edb1ecb54cfca864742a4402f7fa578
Tejeswinigowda <tejeswinidv@gmail.com>
2019-07-20 22:34:00 +0530

    my first commit

---
+++ Demo2/src/test/java/Demo2/Demo2/TestConcat.java
@@ -0,0 +1,17 @@
+package Demo2.Demo2;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConcat {
+
+	@Test
+	void test() {
+		Junit test=new Junit();
+		String result=test.concatenate("one", "two");
+		assertEquals("onetwo",result);
+	}
+
+}
---
+++ Demo2/src/test/java/Demo2/Demo2/testMultiply.java
@@ -0,0 +1,17 @@
+package Demo2.Demo2;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testMultiply {
+
+	@Test
+	void test() {
+		Junit test=new Junit();
+		int result=test.multiply(5, 6);
+		assertEquals(30,result);
+	}
+
+}
